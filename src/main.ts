02ca53ff53cc8600baca234575bc0f981b64f12f
33b3ziz <egyptianhack325@gmail.com>
2023-12-28 16:04:04 +0200

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+// Setting Game Name
+let gameName = "Guess The Word";
+document.title = gameName;
+
+document.querySelector("h1")!.innerHTML = gameName;
+document.querySelector("footer")!.innerHTML =
+  "Guess the word by entering a letter";
